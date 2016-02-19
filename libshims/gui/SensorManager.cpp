8ab28206713ee893a884119714033b72504a4a08
Christopher R. Palmer <crpalmer@gmail.com>
2016-02-19 12:15:47 -0500

    kenzo: libshims: camera: Don't crash if the sensorservice is slow to start
    Right now we wait a bit to find the service and then give up.  If we give
    up then we just (stupidly) immediately crash.  Instead, return an
    error.
    
    Note: This is actually an aosp problem, it was just never triggered
    until cm-13.0.
    
    Change-Id: Ib76c00076bd233ba7dc460588ed32e884795e3eb

--- libshims/gui/SensorManager.cpp
+++ libshims/gui/SensorManager.cpp
@@ -65,14 +65,17 @@ status_t SensorManager::assertStateLocked() const {
     if (mSensorServer == NULL) {
         // try for one second
         const String16 name("sensorservice");
-        status_t err;
+        status_t err = NO_ERROR;
+
         for (int i=0 ; i<4 ; i++) {
-            err = getService(name, &mSensorServer);
-            if (err == NAME_NOT_FOUND) {
+            if (i > 0) {
+                // Don't sleep on the first try or after the last failed try
                 usleep(250000);
-                continue;
             }
-            break;
+            err = getService(name, &mSensorServer);
+            if (err != NAME_NOT_FOUND) {
+                break;
+            }
         }
 
         if (err != NO_ERROR) {
