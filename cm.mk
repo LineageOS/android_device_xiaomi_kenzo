ce80e65b8010eb16fd25c7c09fa07d8acf78523a
TheStrix <parthbhatia98@gmail.com>
2016-03-12 17:42:29 +0530

    kenzo: Add initial files

---
+++ cm.mk
@@ -0,0 +1,43 @@
+#
+# Copyright 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit from kenzo device
+$(call inherit-product, device/xiaomi/kenzo/device.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := kenzo
+PRODUCT_NAME := cm_kenzo
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi Note 3
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Use the latest approved GMS identifiers unless running a signed build
+ifneq ($(SIGN_BUILD),true)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:5.1.1/LMY47V/V7.1.18.0.LHOCNCL:user/release-keys \
+    PRIVATE_BUILD_DESC="kenzo-user 5.1.1 LMY47V V7.1.18.0.LHOCNCL release-keys"
+endif
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo cm_kenzo-userdebug
+add_lunch_combo cm_kenzo-eng
