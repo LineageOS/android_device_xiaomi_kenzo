580b4c32f68e8038393d7f5a567b054883dd45d3
TheStrix <parthbhatia98@gmail.com>
2016-03-13 14:55:39 +0530

    kenzo: Add media configs

--- device.mk
+++ device.mk
@@ -122,7 +122,6 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
