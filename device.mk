49c8947216e66ae03279cb27f4ef1764f02834cc
TheStrix <parthbhatia98@gmail.com>
2016-03-27 22:58:37 +0530

    kenzo: Add required offline changer images

--- device.mk
+++ device.mk
@@ -87,6 +87,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
