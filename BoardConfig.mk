3191e5dd7a52b47d0421bdb99ab1740e65507023
TheStrix <parthbhatia98@gmail.com>
2016-04-04 17:20:40 +0530

    kenzo: Fix derp in defination

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,11 +162,11 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Wifi
+BOARD_WLAN_DEVICE               := qcwcn
 BOARD_HAS_QCOM_WLAN       	:= true
 BOARD_HAS_QCOM_WLAN_SDK   	:= true
 BOARD_HOSTAPD_DRIVER        	:= NL80211
 BOARD_HOSTAPD_PRIVATE_LIB   	:= lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_WLAN_DEVICE           	:= qcwcn
 BOARD_WPA_SUPPLICANT_DRIVER    	:= NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB:= lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 TARGET_USES_WCNSS_CTRL 		:= true
