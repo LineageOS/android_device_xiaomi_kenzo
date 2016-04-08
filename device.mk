76d2c0cb72ecafebe58f0fc2ba280ced520c6c6b
TheStrix <parthbhatia98@gmail.com>
2016-04-08 09:43:27 +0530

    kenzo: Rename media configs for MSM8956 device
    * Also correct permissions for configs to rw-r-r (644)

--- device.mk
+++ device.mk
@@ -150,8 +150,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs_8956.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles_8956.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
