54fe3b6ce4a045a9929e0f92f4b242708b760138
TheStrix <parthbhatia98@gmail.com>
2016-04-27 15:09:52 +0530

    kenzo: Switch to enforcing selinux
    Denials fixed:
    [    3.633911] type=1400 audit(2394309.409:4): avc:  denied  { read } for  pid=341 comm="e2fsck" name="mmcblk0p25" dev="tmpfs" ino=12591 scontext=u:r:fsck:s0 tcontext=u:object_r:block_device:s0 tclass=blk_file permissive=0
    [    3.633979] type=1400 audit(2394309.409:5): avc:  denied  { read write } for  pid=341 comm="e2fsck" name="mmcblk0p25" dev="tmpfs" ino=12591 scontext=u:r:fsck:s0 tcontext=u:object_r:block_device:s0 tclass=blk_file permissive=0
    [    4.962593] init: avc:  denied  { set } for property=ctl.rmt_storage scontext=u:r:qti_init_shell:s0 tcontext=u:object_r:ctl_default_prop:s0 tclass=property_service
    [    4.980617] init: avc:  denied  { set } for property=ctl.rfs_access scontext=u:r:qti_init_shell:s0 tcontext=u:object_r:ctl_default_prop:s0 tclass=property_service
    [    6.326787] type=1400 audit(1461744488.379:6): avc: denied { execmod } for pid=387 comm="mediaserver" path="/system/lib/libDiracAPI_SHARED.so" dev="mmcblk0p24" ino=1209 scontext=u:r:mediaserver:s0 tcontext=u:object_r:system_file:s0 tclass=file permissive=0
    [   18.623994] type=1400 audit(1461744500.679:7): avc: denied { read } for pid=992 comm="system_server" name="properties" dev="mmcblk0p26" ino=59 scontext=u:r:system_server:s0 tcontext=u:object_r:persist_file:s0 tclass=dir permissive=0
    [  115.685363] type=1400 audit(1461754492.140:4): avc: denied { search } for pid=5072 comm="hci_qcomm_init" name="qmux_bluetooth" dev="tmpfs" ino=12796 scontext=u:r:qti_init_shell:s0 tcontext=u:object_r:qmuxd_socket:s0 tclass=dir permissive=0
    [ 1236.592804] type=1400 audit(1461752827.894:4): avc: denied { getattr } for pid=5669 comm="sensorservice" path="/dev/sensors" dev="tmpfs" ino=12536 scontext=u:r:shell:s0 tcontext=u:object_r:sensors_device:s0 tclass=chr_file permissive=0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x00000100
