357f1c356c4c688074a0b73351e7079f11cf242f
Adam Lesinski <adamlesinski@google.com>
2016-05-01 22:19:14 -0700

    kenzo: Remove density from PRODUCT_AAPT_CONFIG
    AAPT ignores densities in PRODUCT_AAPT_CONFIG.
    The use of PRODUCT_AAPT_PREF_CONFIG for density is
    encouraged, as AAPT is able to determine the fallback
    density to use if a resource of the specified density
    does not exist.
    
    Change-Id: If2b43a44593bc234118045d03bd48cd9820dc05a

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/kenzo/kenzo-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Screen density
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Boot animation
