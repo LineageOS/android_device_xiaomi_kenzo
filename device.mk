d2f23d68493442bfda010438261e3d31abe665ae
TheStrix <parthbhatia98@gmail.com>
2016-05-12 17:27:59 +0530

    kenzo: Add media codecs performance

--- device.mk
+++ device.mk
@@ -152,6 +152,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
