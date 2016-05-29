c9a40095bc7d12ff83fd00ffa319c32044059271
TheStrix <parthbhatia98@gmail.com>
2016-05-29 10:44:06 +0530

    kenzo: Build Snap camera

--- device.mk
+++ device.mk
@@ -205,6 +205,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
 
+# Snapdragon Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # Sound trigger
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
