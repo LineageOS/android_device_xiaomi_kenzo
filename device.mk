e88f9739158618ea0709cdc8974471f8154a8fbc
TheStrix <parthbhatia98@gmail.com>
2016-06-19 01:52:52 -0400

    kenzo: Ship Gello browser
    Change-Id: I2354b74520f88a6d0b67402808c8a74255cd40cb

--- device.mk
+++ device.mk
@@ -91,6 +91,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_wcd9326.xml
 
+# Browser
+PRODUCT_PACKAGES += \
+    Gello
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
