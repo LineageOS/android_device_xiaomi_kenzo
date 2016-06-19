77710fbe9a915d14bf7c694b1cc4b2bc3b1db271
TheStrix <parthbhatia98@gmail.com>
2016-06-19 06:56:43 -0400

    Revert "kenzo: Add IMS configs"
    * It never worked
    This reverts commit 21c1ad1e03c5d475b921179a343a7d4d75fe3a75.
    
    Change-Id: I082e65504c24deeb42ef572766ada1d3d2ff3494

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -68,9 +68,7 @@ done
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \\
     CNEService \\
-    dpmserviceapp \\
-    ims \\
-    imssettings
+    dpmserviceapp
 
 PRODUCT_PACKAGES += \\
     com.qualcomm.location
@@ -155,28 +153,6 @@ LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := ims
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/vendor/app/ims/ims.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
-LOCAL_PROPRIETARY_MODULE := true
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := imssettings
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/vendor/app/imssettings/imssettings.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
-LOCAL_PROPRIETARY_MODULE := true
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
