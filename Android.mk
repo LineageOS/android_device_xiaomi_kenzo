96126a4c1890aac49def6bb47c2e1129f8f29c63
TheStrix <parthbhatia98@gmail.com>
2016-06-27 10:45:17 +0530

    kenzo: Add missing IMS blob
    * Fix symlink path
    * Cleanup
    
    Change-Id: I9108585eea6c4cc52d1f967373e1301e1599171a

--- Android.mk
+++ Android.mk
@@ -211,7 +211,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(WV_SYMLINKS)
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT)/app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
