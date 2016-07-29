572d6ee5b616a1d020902a430679a13528d485ec
TheStrix <parthbhatia98@gmail.com>
2016-07-29 23:57:58 +0530

    kenzo: Update keylayouts
    * Add VIRTUAL flag for kenzo (Credits: @Alexey71)
    * Remove deprecated WAKE flag from msm8976-tashalite-snd-card_Button_Jack.kl
    * Remove unused keylayouts
    
    Change-Id: Ie944c0925a97c68ca6d7c23323fc6c8aec05fa1d

--- device.mk
+++ device.mk
@@ -154,9 +154,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/Atmel_maXTouch_Touchscreen.kl:system/usr/keylayout/Atmel_maXTouch_Touchscreen.kl \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl:system/usr/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
+    $(LOCAL_PATH)/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl:system/usr/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl
 
 # Keymaster
 PRODUCT_PACKAGES += \
