52fa7e26dea0ff2a438b3f1ab1555e10134b6c15
TheStrix <parthbhatia98@gmail.com>
2016-08-10 19:56:04 +0530

    kenzo: Update audio
    * Enable fluence
    * Enable sound trigger
    * Enable QCOM audio features
    * Update audio configs and ACDB
    
    Change-Id: Ib5aa02c470a115e70a08f08c78ddf0022e8f4089

--- device.mk
+++ device.mk
@@ -88,6 +88,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/diracmobile.config:system/vendor/etc/diracmobile.config \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_wcd9326.xml
 
 # Browser
