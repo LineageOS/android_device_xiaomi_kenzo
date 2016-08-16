724154e27ca2bf6be8c3122f42682bbd292444fb
TheStrix <parthbhatia98@gmail.com>
2016-08-16 23:31:41 +0530

    kenzo: init: Set dalvik properties dynamically
    * Picked from http://review.cyanogenmod.org/#/c/156572/
    * Add libinit_kenzo
    * Remove duplicate HWUI props (Already set via frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
    
    Change-Id: I269ca77c81e431acd05884f93ee00680fa9e1774

--- device.mk
+++ device.mk
@@ -31,8 +31,6 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Permissions
