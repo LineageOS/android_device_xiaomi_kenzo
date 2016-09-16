a6e26c6d3c8700b5998bb582295ac9b2fb7d1d9b
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-09-16 00:38:37 -0400

    kenzo: Add missing ANT+ permission
    * Required to advertise ANT+ support
    
    Change-Id: Id409b4657d22b915d6f7854f918b00d94c8a61e4

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
