929def558e6713bc71a4d468ab70b6cfe3a0bfa0
Steve Kondik <steve@cyngn.com>
2016-10-14 21:18:03 -0700

    kenzo: Bring DozeSettings up to date
     * Rework the UI with a SwitchBar
     * Change to the preferences support library and use fragments
     * Use all the new fancy RemotePreference stuff
     * Slot into display settings instead of the dashboard
    
    Change-Id: I0850f7d9558a14f739f752f76002db993381a0e5

--- doze/Android.mk
+++ doze/Android.mk
@@ -8,8 +8,28 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := XiaomiDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4 \
+    android-support-v13 \
+    android-support-v7-recyclerview \
+    android-support-v7-preference \
+    android-support-v7-appcompat \
+    android-support-v14-preference \
+    org.cyanogenmod.platform.internal
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    frameworks/support/v7/preference/res \
+    frameworks/support/v14/preference/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/recyclerview/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
 
--- doze/src/com/cyanogenmod/settings/doze/DozeSettings.java
+++ doze/src/com/cyanogenmod/settings/doze/DozeSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,75 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.cyanogenmod.settings.doze;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
-import android.provider.Settings;
-import android.view.Menu;
 import android.view.MenuItem;
 
-public class DozeSettings extends PreferenceActivity implements OnPreferenceChangeListener {
+import com.android.settingslib.drawer.SettingsDrawerActivity;
 
-    private Context mContext;
-    private SharedPreferences mPreferences;
+/**
+ * Created by shade on 10/14/16.
+ */
 
-    private SwitchPreference mAmbientDisplayPreference;
-    private SwitchPreference mPickUpPreference;
-    private SwitchPreference mHandwavePreference;
-    private SwitchPreference mPocketPreference;
+public class DozeSettings extends SettingsDrawerActivity {
+
+    private static final String TAG_DOZE = "doze";
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.doze_settings);
-        mContext = getApplicationContext();
-        boolean dozeEnabled = Utils.isDozeEnabled(mContext);
 
-        // get shared preference
-        mPreferences = mContext.getSharedPreferences("doze_settings", Activity.MODE_PRIVATE);
-        if (savedInstanceState == null && !mPreferences.getBoolean("first_help_shown", false)) {
-            showHelp();
-        }
+        setContentView(R.layout.doze);
 
-        mAmbientDisplayPreference =
-            (SwitchPreference) findPreference(Utils.AMBIENT_DISPLAY_KEY);
-        // Read from DOZE_ENABLED secure setting
-        mAmbientDisplayPreference.setChecked(dozeEnabled);
-        mAmbientDisplayPreference.setOnPreferenceChangeListener(this);
-
-        mPickUpPreference =
-            (SwitchPreference) findPreference(Utils.PICK_UP_KEY);
-        mPickUpPreference.setOnPreferenceChangeListener(this);
-
-        mHandwavePreference =
-            (SwitchPreference) findPreference(Utils.GESTURE_HAND_WAVE_KEY);
-        mHandwavePreference.setOnPreferenceChangeListener(this);
-
-        mPocketPreference =
-            (SwitchPreference) findPreference(Utils.GESTURE_POCKET_KEY);
-        mPocketPreference.setOnPreferenceChangeListener(this);
-
-        final ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new DozeSettingsFragment(), TAG_DOZE).commit();
     }
 
     @Override
@@ -91,58 +46,5 @@ public class DozeSettings extends PreferenceActivity implements OnPreferenceChan
             return true;
         }
         return false;
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        final String key = preference.getKey();
-        final boolean value = (Boolean) newValue;
-        if (Utils.AMBIENT_DISPLAY_KEY.equals(key)) {
-            mAmbientDisplayPreference.setChecked(value);
-            Utils.enableDoze(value, mContext);
-            return true;
-        } else if (Utils.PICK_UP_KEY.equals(key)) {
-            mPickUpPreference.setChecked(value);
-            Utils.startService(mContext);
-            return true;
-        } else if (Utils.GESTURE_HAND_WAVE_KEY.equals(key)) {
-            mHandwavePreference.setChecked(value);
-            Utils.startService(mContext);
-            return true;
-        } else if (Utils.GESTURE_POCKET_KEY.equals(key)) {
-            mPocketPreference.setChecked(value);
-            Utils.startService(mContext);
-            return true;
-        }
-        return false;
-    }
-
-    public static class HelpDialogFragment extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            return new AlertDialog.Builder(getActivity())
-                    .setTitle(R.string.doze_settings_help_title)
-                    .setMessage(R.string.doze_settings_help_text)
-                    .setNegativeButton(R.string.dlg_ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-                        }
-                    })
-                    .create();
-        }
-
-        @Override
-        public void onCancel(DialogInterface dialog) {
-            getActivity().getSharedPreferences("doze_settings", Activity.MODE_PRIVATE)
-                    .edit()
-                    .putBoolean("first_help_shown", true)
-                    .commit();
-        }
-    }
-
-    private void showHelp() {
-        HelpDialogFragment fragment = new HelpDialogFragment();
-        fragment.show(getFragmentManager(), "help_dialog");
     }
 }
