1141fc2ff3e5be9efbbaca9f68686cb21232389c
LuK1337 <priv.luk@gmail.com>
2017-01-26 00:48:19 +0100

    msm8956: Add libc hax for camera
     * See : https://review.lineageos.org/#/c/1170/
    
    Change-Id: I040c3023f81322140d153cb14be9578457777ac2

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -25,3 +25,18 @@ LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    bionic/bionic_time_conversions.cpp \
+    bionic/pthread_cond.cpp
+
+LOCAL_SHARED_LIBRARIES := libc
+
+LOCAL_MODULE := libshims_camera
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
