fc609355a30f2b8d39864fd60796390825072574
nicknitewolf <nicknitewolf@gmail.com>
2017-02-27 20:30:09 +0800

    kenzo: Use MIUI Global Stable build fingerprint
     * Safetynet throws a CTS profile mismatch error
       while using MIUI Dev build fingerprint
    
    Change-Id: I00d21c5556430266317c380e610c2a3345871124

--- lineage.mk
+++ lineage.mk
@@ -34,6 +34,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/6.11.24:user/release-keys \
-    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M 6.11.24 release-keys"
+    BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V8.2.1.0.MHOCNDL:user/release-keys \
+    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V8.2.1.0.MHOCNDL release-keys"
 endif
