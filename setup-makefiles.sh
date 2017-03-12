e343206afb27c9a7e154442c10c695906676b567
TheStrix <parthbhatia@lineageos.org>
2017-03-12 13:06:49 +0530

    kenzo: Use extractors from msm8956-common
    Change-Id: Icfe22748f6e7d3afd40c653ee4be7995259dfb03

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,30 +19,10 @@
 set -e
 
 # Required!
-DEVICE=kenzo
-VENDOR=xiaomi
+export DEVICE=kenzo
+export DEVICE_COMMON=msm8956-common
+export VENDOR=xiaomi
 
-# Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+export DEVICE_BRINGUP_YEAR=2016
 
-CM_ROOT="$MY_DIR"/../../..
-
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-# The blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
-
-# We are done!
-write_footers
+./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
