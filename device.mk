db84767de395858dfe4400d3da9ec0bd5a152f4d
TheStrix <parthbhatia@lineageos.org>
2017-03-19 15:38:17 +0530

    kenzo: Movie libshims from msm8956-common
    Change-Id: Ic932d445b1ec3a90ddf194dccb57b7fe12014a97

--- device.mk
+++ device.mk
@@ -36,6 +36,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl:system/usr/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl
 
+# Libshims
+PRODUCT_PACKAGES += \
+    libshims_ims
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.target.rc
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
