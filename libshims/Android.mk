f04401b928ac1a7240a777fc1e0c168b240ba207
Billchen1977 <billchen1977@gmail.com>
2017-09-18 17:43:51 +0530

    kenzo: Update ims shim
     * Reformat libshims makefile
    
    Change-Id: I0a3e0ed6c8c97d87dda5cd2d668e2f464c6f62c8

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,29 +16,21 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := media/libstagefright/foundation/MediaBuffer.c
-
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-
+LOCAL_SRC_FILES := media/libstagefright/foundation/MediaBuffer.cpp
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation libui libgui
+LOCAL_CFLAGS := -Wno-unused-private-field
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     bionic/bionic_time_conversions.cpp \
     bionic/pthread_cond.cpp
-
 LOCAL_SHARED_LIBRARIES := libc
-
 LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_32_BIT_ONLY := true
-
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
